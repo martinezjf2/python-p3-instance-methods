408ba82b79dce27feac25dcad281dc14aeb6c2d9
Jeffrey Martinez <martinezjf2@gmail.com>
2023-03-20 20:48:37 -0400

    Done

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,10 @@
 class Person:
     # Class body goes here
 
+    def talk(self):
+        print("Hello World!")
     #Instance method definition
+    
+    def walk(self):
+        print("The person is walking.")
     pass
